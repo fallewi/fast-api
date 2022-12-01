7140add78ec543beaa2b961fd732a2acd62aca8e
fall lewis <fallewi@gmail.com>
2022-12-01 09:39:35 +0100

    ajout

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    message = f"Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
+    message = f"Hello Fall! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
         return {"message": message}
