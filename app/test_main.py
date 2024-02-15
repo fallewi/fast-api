e54cfef6a4d84b25357d7a1c2da2170196e23e51
fall lewis <fall-lewis.y@datascientest.com>
2024-02-15 14:16:22 +0100

    fin de la MC DEVOPS

--- app/test_main.py
+++ app/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "MC DEVOPS"}
+    assert response.json() == {"msg": "FIN MC DEVOPS"}
