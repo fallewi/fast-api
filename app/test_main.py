f65de474778186628625deb2964b0f4f739f133a
fall lewis <fall-lewis.y@datascientest.com>
2025-03-14 13:52:32 +0100

    correction de l'erreur de test

--- app/test_main.py
+++ app/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "FIN DE LA DEMO DEVOPS"}
+    assert response.json() == {"msg": "DEBUT DE LA DEMO DEVOPS"}
